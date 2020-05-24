47dbaf99aaa6841e38a193077d9b74eb9af3fa0a
Anshu <raianshu1998@gmail.com>
2020-05-24 12:23:30 +0530

    added posts reducer

--- src/redux/reducers/all-reducers.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-const reducers = combineReducers({
-    test() {
-        return 'Test value'
-    }
-});
-
-export default reducers;
---
+++ src/redux/reducers/posts.js
@@ -0,0 +1,4 @@
+export default function posts(state = []) {
+    console.log('in post',state);
+  return state;
+}
